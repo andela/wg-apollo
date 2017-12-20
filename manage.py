b4fb8642f161730ce1b4b0818ea1c88f485f385d
Arnold Taremwa <taremwaarnold@gmail.com>
2017-12-20 21:01:55 +0300

    [Feature #153505619] Change path to settings file

--- manage.py
+++ manage.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 import sys
 
+import os
 from django.core.management import execute_from_command_line
 
 from tasks import (
-    setup_django_environment,
-    get_user_config_path
+    setup_django_environment
 )
 
 if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
     if not any('--settings' in s for s in sys.argv):
-        setup_django_environment(get_user_config_path('wger', 'settings.py'))
-
+        BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+        setup_django_environment(os.path.join(BASE_DIR, 'settings.py'))
     # Alternative to above
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
